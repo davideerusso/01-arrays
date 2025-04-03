ef2e3533c9db4d6254bd9fe0218d33122b88bffe
davideerusso <rd.96.rd@gmail.com>
2025-04-03 15:54:13 +0200

    3

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ const fifthTeacher = teachers[4];
 console.log(fifthTeacher);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+
+console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
